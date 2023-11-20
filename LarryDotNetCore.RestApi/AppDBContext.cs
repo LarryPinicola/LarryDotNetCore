feb0ba97956d379f3d3915d7db4ae0ac91a84fff
LarryPinicola <thelarry1549@gmail.com>
2023-11-20 16:49:10 +0630

    [complete] RestApi Read, Edit, Create

---
+++ LarryDotNetCore.RestApi/AppDBContext.cs
@@ -0,0 +1,32 @@
+﻿using LarryDotNetCore.RestApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarryDotNetCore.RestApi
+{
+    public class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+                {
+                    DataSource = "DESKTOP-47QMMLG",
+                    InitialCatalog = "Tbl_Blog",
+                    UserID = "sa",
+                    Password = "sa@123",
+                    TrustServerCertificate = true
+                };
+                optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+            }
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
---
+++ LarryDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,93 @@
+﻿using LarryDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LarryDotNetCore.RestApi.Controllers
+{
+    // api/blog
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            try
+            {
+                AppDBContext db = new AppDBContext();
+                List<BlogDataModel> lst = db.Blogs.ToList();
+
+                /*BlogListResponseModel model = new BlogListResponseModel();
+                model.IsSuccess = true;
+                model.Message = "success";
+                model.Data = lst;*/
+                //return Ok("GetBlogs");
+
+                BlogListResponseModel model = new BlogListResponseModel
+                {
+                    IsSuccess = true,
+                    Message = "success",
+                    Data = lst,
+                };
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(new BlogListResponseModel
+                {
+                    IsSuccess = false,
+                    //Message = ex.ToString() //detail error message
+                    Message = ex.Message, //summery error message
+                });
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EditBlog(int id)
+        {
+            AppDBContext db = new AppDBContext();
+            var item = db.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                var response = new { IsSuccess = false, Message = "No data found" };
+                return NotFound(response);
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogDataModel blog)
+        {
+            AppDBContext db = new AppDBContext();
+            db.Blogs.Add(blog);
+            var result = db.SaveChanges();
+            BlogResponseModel model = new BlogResponseModel()
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "saving successful." : "saving fail.",
+                Data = blog,
+            };
+            return Ok(model);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlog()
+        {
+            return Ok("UpdateBlog");
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlog()
+        {
+            return Ok("PatchBlog");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlog()
+        {
+            return Ok("DeleteBlog");
+        }
+    }
+}
