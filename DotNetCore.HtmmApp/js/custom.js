d30db381588925e3988b2ceffaf06fee11ebec4e
LarryPinicola <thelarry1549@gmail.com>
2023-11-30 13:16:53 +0630

    jQuery Read, create, edit and delete with action btns

---
+++ DotNetCore.HtmmApp/js/custom.js
@@ -0,0 +1,42 @@
+// success alertbox from sweetalert
+function successMessage(message) {
+  Swal.fire({
+    title: "Success!",
+    text: message, // give message as a parameter and customize at the other components
+    icon: "success",
+  });
+}
+
+// Warning alertBox from sweetalert
+function warningMessage(message) {
+  Swal.fire({
+    title: "Warning!",
+    text: message,
+    icon: "warning",
+  });
+}
+
+// uuid from stackoverflow (uiniversal unique id)
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
+// delete alertBox from sweetalert
+function confirmMessage(message) {
+  return new Promise((resolve, reject) => {
+    Swal.fire({
+      title: "Confirm",
+      text: message,
+      icon: "warning",
+      showCancelButton: true,
+    }).then((result) => {
+      // return result.isConfirmed;
+      resolve(result.isConfirmed);
+    });
+  });
+}
