fac9e84cbe9a805ee746aaa8842f78161a9d09b9
Larry <thelarry1549@gmail.com>
2023-12-30 12:54:05 +0630

    -

---
+++ LarryDotNetCore.MVCTheme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LarryDotNetCore.MVCTheme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LarryDotNetCore.ThemeMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LarryDotNetCore.ThemeMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
