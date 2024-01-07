e35df6a1b828d586d157ceb63d0bedecccfdfc9a
LarryPinicola <thelarry1549@gmail.com>
2024-01-07 20:30:13 +0630

    [completed] deleted unnecessary project

--- dotNetCoreThemeMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dotNetCoreThemeMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
