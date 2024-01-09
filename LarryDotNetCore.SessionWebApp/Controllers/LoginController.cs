97ed455515e273ebb56808d6c51a867cbddaa1b3
LarryPinicola <thelarry1549@gmail.com>
2024-01-09 14:23:13 +0630

    [InProgress] Session webApp

---
+++ LarryDotNetCore.SessionWebApp/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using LarryDotNetCore.SessionWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LarryDotNetCore.SessionWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel reqModel)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ LarryDotNetCore.SessionWebApp/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LarryDotNetCore.SessionWebApp.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
