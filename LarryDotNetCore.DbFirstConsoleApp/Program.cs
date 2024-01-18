82ef76eada3ae77ade890aa900ca5b06389beaf1
Larry <thelarry1549@gmail.com>
2024-01-18 11:33:25 +0630

    [complete] Db First PM command Console App

---
+++ LarryDotNetCore.DbFirstConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Scaffold DbContext Command on PM (package manager)
+
+/*Scaffold-DbContext [-Connection] [-Provider] [-OutputDir] [-Context] [-Schemas>] [-Tables>] 
+-DataAnnotations][-Force][-Project][-StartupProject][<CommonParameters>]*/
+
+// Scaffold-DbContext "Server=.;Database=LarryDotNetCore;User ID=sa;Password=sa@123;Trusted_Connection=True;Trust Server Certificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context AppDbContext -Tables Tbl_Blog
