7df22549b7e9a3cec79da1007c361dbefae3ffed
LarryPinicola <thelarry1549@gmail.com>
2024-01-18 20:09:06 +0630

    -

--- LarryDotNetCore.ThemeMVCApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LarryDotNetCore.ThemeMVCApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
