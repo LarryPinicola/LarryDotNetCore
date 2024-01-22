45e6fabe21133d960a468c0f68c9ec40a00cdd35
HtetAung-ITGuy <htetaung.general@gmail.com>
2024-01-22 16:21:56 +0630

    [complete] SignalR chatApp

---
+++ LarryDotNetCore.SignalRChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LarryDotNetCore.SignalRChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
